d7ec0b991b816a76bfa402fc4d180d061a23ccba
Einar Logi Óskarsson <einarlogi1@gmail.com>
2024-12-02 21:23:32 +0000

    Pushing the project to repo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hi, I'm Einar Logi!</h1>
+      <p>An aspiring full stack developer</p>
+    </>
+  )
+}
+
+export default App
